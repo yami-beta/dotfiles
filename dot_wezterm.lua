4155712aae300ba029edf8d8eeed6d9f83a9b62c
yami-beta <yami-beta@users.noreply.github.com>
2025-01-15 17:02:50 +0900

    feat: update wezterm config
    Ghostty を試していた時に良かったので pane 操作のキーバインドを変更
    - pane 分割を `SUPER + d`, `SUPER + SHIFT + d` にした
    - pane 移動を `SUPER + [`, `SUPER + ]` にした`
    
    `wezterm.on` の設定をファイル下部に移動した
    イベントハンドラで頻繁に確認することも無いので

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,32 +1,6 @@
 local wezterm = require 'wezterm'
-
-wezterm.on('update-right-status', function(window, pane)
-  window:set_right_status(window:active_workspace())
-end)
-
--- https://wezfurlong.org/wezterm/config/lua/window/set_config_overrides.html を参考に
-wezterm.on('toggle-opacity', function(window, pane)
-  local overrides = window:get_config_overrides() or {}
-
-  -- set_config_overrides は文字通り config に対して上書きする値を設定する
-  -- 透過度は `config.window_background_opacity = 0.9` を設定しているので
-  -- `overrides.window_background_opacity == nil` のとき透過が適用され
-  -- `overrides.window_background_opacity == 1.0` で未透過になる
-  --
-  -- lua はテーブルで nil を割り当てるとキーが存在しないことと同じ扱いになるため
-  -- `overrides.window_background_opacity = nil` は `window_background_opacity` の上書きをしないことになるらしい
-  if overrides.window_background_opacity then
-    overrides.window_background_opacity = nil
-  else
-    overrides.window_background_opacity = 1.0
-  end
-
-  window:set_config_overrides(overrides)
-end)
-
-local config = {}
-
 local act = wezterm.action
+local config = {}
 
 config.keys = {
   {
@@ -68,19 +42,29 @@ config.keys = {
     },
   },
   {
-    key = '\\',
-    mods = 'ALT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    key = 'd',
+    mods = 'SUPER',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
   {
-    key = '-',
-    mods = 'ALT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    key = 'd',
+    mods = 'SUPER|SHIFT',
+    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '[',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Prev'
+  },
+  {
+    key = ']',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Next'
   },
   {
     key = 'u',
     mods = 'SUPER',
-    action = wezterm.action.EmitEvent 'toggle-opacity',
+    action = act.EmitEvent 'toggle-opacity',
   },
 }
 
@@ -90,6 +74,9 @@ config.color_scheme = 'OneHalfDark'
 -- config.color_scheme = 'nightfox'
 -- config.color_scheme = 'nordfox'
 -- config.color_scheme = 'duskfox'
+-- config.color_scheme = 'Catppuccin Frappe'
+-- config.color_scheme = 'Catppuccin Macchiato'
+-- config.color_scheme = 'Catppuccin Mocha'
 
 -- https://zenn.dev/paiza/articles/9ca689a0365b05
 config.font = wezterm.font_with_fallback({
@@ -100,13 +87,34 @@ config.font_size = 16.0
 
 config.window_background_opacity = 0.9
 
--- https://wezfurlong.org/wezterm/config/lua/config/cursor_blink_rate.html
-config.cursor_blink_rate = 0
-
 -- https://wezfurlong.org/wezterm/config/appearance.html#styling-inactive-panes
 config.inactive_pane_hsb = {
-  -- saturation = 0.8,
+  saturation = 0.8,
   brightness = 0.6,
 }
+
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
+
+-- https://wezfurlong.org/wezterm/config/lua/window/set_config_overrides.html を参考に
+wezterm.on('toggle-opacity', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+
+  -- set_config_overrides は文字通り config に対して上書きする値を設定する
+  -- 透過度は `config.window_background_opacity = 0.9` を設定しているので
+  -- `overrides.window_background_opacity == nil` のとき透過が適用され
+  -- `overrides.window_background_opacity == 1.0` で未透過になる
+  --
+  -- lua はテーブルで nil を割り当てるとキーが存在しないことと同じ扱いになるため
+  -- `overrides.window_background_opacity = nil` は `window_background_opacity` の上書きをしないことになるらしい
+  if overrides.window_background_opacity then
+    overrides.window_background_opacity = nil
+  else
+    overrides.window_background_opacity = 1.0
+  end
+
+  window:set_config_overrides(overrides)
+end)
 
 return config
